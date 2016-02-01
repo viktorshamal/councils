35f61a603ae135f91c050ef7cd7fe998b1353493
Viktor Shamal Andersen <Sphinxominator@gmail.com>
2016-02-01 22:34:25 +0100

    annotated some more

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,6 +1,5 @@
 var councils = angular.module('councils',[
     'templates',
-    //ngRoute',
     'controllers',
     'restmod',
     'monospaced.qrcode',
@@ -16,78 +15,44 @@ var councils = angular.module('councils',[
     restmodProvider.rebase('AMSApi');
     restmodProvider.rebase({
         $config: {
-            urlPrefix: '/v1' // or use setProperty('urlPrefix', '/api/v1') in a definition function
+            urlPrefix: '/v1'
         }
     });
 }])
+.config(['$stateProvider','$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/");
 
-/*.config(['$routeProvider', function($routeProvider) {
-    $routeProvider
-        .when('/documents/:id', {
-            templateUrl: 'documents/show.html',
-            controller: 'DocumentController'
-        })
-        .when('/documents/', {
-            templateUrl: 'documents/index.html',
-            controller: 'DocumentIndexController'
-        })
-        .when('/:council/meetings/:id', {
-        templateUrl: 'meetings/show.html',
-        controller: 'MeetingController'
-        })
-        .when('/:council/meetings/', {
-        templateUrl: 'meetings/index.html',
-        controller: 'MeetingIndexController'
-        })
-        .when('/', {
-        templateUrl: 'meetings/index.html',
-        controller: 'MeetingIndexController'
-        })
-        .when('/attend/:secret', {
-            resolve: {
-                attend: ['attendService', function (attendService) {
-                    attendService();
-                }]
+    $stateProvider
+        .state('root', {
+            url: "",
+            views: {
+                'header': {
+                    templateUrl: "header.html",
+                    controller: 'headerController'
+                },
+                'body': {
+                    templateUrl: "<div ui-view></div>"
+                }
             }
         })
-  }
-])*/
-    .config(function($stateProvider, $urlRouterProvider) {
-        $urlRouterProvider.otherwise("/");
-
-        $stateProvider
-            .state('root', {
-                url: "",
-                views: {
-                    'header': {
-                        templateUrl: "header.html",
-                        controller: 'headerController'
-                    },
-                    'body': {
-                        templateUrl: "<div ui-view></div>"
-                    }
-                }
-            })
-            .state('root.councils', {
-                url: "/c/:council",
-                templateUrl: "council/councils.html"
-            })
-            .state('root.councils.meetings', {
-                url: "/meetings",
-                templateUrl: "meetings/meetings.html"
-            })
-            .state('root.councils.meetings.list',{
-                url: "/",
-                templateUrl: "meetings/index.html",
-                controller: 'MeetingIndexController'
-            })
-            .state('root.councils.meetings.show', {
-                url: "/:id",
-                templateUrl: "meetings/show.html",
-                controller: "MeetingController"
-            });
-    }).run(function($state){
-       console.log($state.get());
-    });
+        .state('root.councils', {
+            url: "/c/:council",
+            templateUrl: "council/councils.html"
+        })
+        .state('root.councils.meetings', {
+            url: "/meetings",
+            templateUrl: "meetings/meetings.html"
+        })
+        .state('root.councils.meetings.list',{
+            url: "/",
+            templateUrl: "meetings/index.html",
+            controller: 'MeetingIndexController'
+        })
+        .state('root.councils.meetings.show', {
+            url: "/:id",
+            templateUrl: "meetings/show.html",
+            controller: "MeetingController"
+        });
+}]);
 
 controllers = angular.module('controllers',[]);
