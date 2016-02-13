af12221a57aaae121b90cf45e027ed0ffdd72674
Viktor Shamal Andersen <Sphinxominator@gmail.com>
2016-02-13 10:47:37 +0100

    Hide navigation if no council

--- app/assets/javascripts/components/header/headerController.js
+++ app/assets/javascripts/components/header/headerController.js
@@ -1,3 +1,7 @@
 councils.controller('headerController', ['$scope','$state','$auth', function($scope,$state,$auth) {
-    //$scope.identifier = $auth.user.council.identifier;
+    $scope.$watchCollection(function(){
+        return $state.params.council;
+    }, function(){
+        $scope.councilChosen = !!$state.params.council;
+    });
 }]);
