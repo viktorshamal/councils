15bdafa5f71be704a2c047363f1a70ad699c7226
Viktor Shamal Andersen <Sphinxominator@gmail.com>
2016-02-20 20:56:06 +0100

    Clean up meeting model

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,7 +1,7 @@
 class Meeting < ActiveRecord::Base
   attr_accessor :name, :color
 
-  validates_presence_of :secret, :name, :meeting_template_id, :agenda_drive_id, :summary_drive_id
+  validates_presence_of :secret, :meeting_template_id, :agenda_drive_id, :summary_drive_id
 
   belongs_to :meeting_template
   belongs_to :council
@@ -9,24 +9,32 @@ class Meeting < ActiveRecord::Base
   has_many :attendances
   has_many :users, through: :attendances
 
+  after_initialize :inherit_attributes
   before_validation(on: :create) do
     self.secret = SecureRandom.base64(10)
-
-    self.agenda_drive_id = DriveWrapper.create_public_document({
-      name: "Agenda: #{self.date.strftime('%A, %e. %B %Y')}",
-      mime_type: 'application/vnd.google-apps.document',
-    }, Meeting.editors)
-
-    self.summary_drive_id = DriveWrapper.create_public_document({
-      name: "Summary: #{self.date.strftime('%A, %e. %B %Y')}",
-      mime_type: 'application/vnd.google-apps.document'
-    }, Meeting.editors)
+    create_google_documents
   end
 
-  after_initialize :inherit_attributes
+  private
+  def create_google_documents
+    unless self.agenda_drive_id
+      self.agenda_drive_id = DriveWrapper.create_public_document({
+         name: "Dagsorden: #{self.date.strftime('%A, %e. %B %Y')}",
+         mime_type: 'application/vnd.google-apps.document',
+     }, Meeting.editors)
+    end
+
+    unless self.summary_drive_id
+      self.summary_drive_id = DriveWrapper.create_public_document({
+          name: "Referat: #{self.date.strftime('%A, %e. %B %Y')}",
+          mime_type: 'application/vnd.google-apps.document'
+      }, Meeting.editors)
+    end
+  end
 
   def inherit_attributes
-    [:name, :color].each do |attr|
+    inherited_attributes = [:name, :color]
+    inherited_attributes.each do |attr|
       next unless self.respond_to?(attr.to_s) && self.meeting_template.respond_to?(attr.to_s)
       self.send(attr.to_s + '=', self.meeting_template.send(attr).to_s)
     end
