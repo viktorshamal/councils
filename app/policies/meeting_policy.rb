7679ea7a6819fd1fa0290632ea13bcee168f0a57
Viktor Shamal Andersen <Sphinxominator@gmail.com>
2016-02-24 20:23:17 +0100

    Fix admins not being able to create meetings

--- app/policies/meeting_policy.rb
+++ app/policies/meeting_policy.rb
@@ -1,13 +1,13 @@
 class MeetingPolicy < ApplicationPolicy
   def create?
-    user.has_role? :moderator, record.meeting_template
+    user.is_admin? || user.has_role?(:moderator, record.meeting_template)
   end
 
   def update?
-    user.has_role? :moderator, record.meeting_template
+    user.is_admin? || user.has_role?(:moderator, record.meeting_template)
   end
 
   def destroy?
-    user.has_role? :moderator, record.meeting_template
+    user.is_admin? || user.has_role?(:moderator, record.meeting_template)
   end
 end
