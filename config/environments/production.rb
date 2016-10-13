454a7968f8e55a8385c2829833ed82de43b69632
Viktor Shamal Andersen <Sphinxominator@gmail.com>
2016-10-13 17:15:39 +0200

    disable uglifier and sass rails compressor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,9 +18,9 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-  # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  config.assets.css_compressor = :sass
+  # Compress JavaScripts and CSS. Not needed. Webpack takes care of this.
+  #config.assets.js_compressor = :uglifier
+  #config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
